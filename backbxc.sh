d6c20277d250750470a4bad2b26f0877f7627547
sxzcy <52669443+sxzcy@users.noreply.github.com>
2019-08-21 20:27:37 +0800

    add

--- backbxc.sh
+++ backbxc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get install curlftpfs tar -y
-curlftpfs -o codepage=gbk ftp://ftp1:biwang@chub.i234.me:2221 /mnt
+curlftpfs -o codepage=gbk ftp://root:biwang123@47.88.220.107 /mnt
 tar -czvf /mnt/biwang/bcode/$(cat /var/lib/docker/volumes/bxc_data/_data/node.db | awk -F '"' '{print $12}').tar.gz -C /var/lib/docker/volumes/ bxc_data
 umount /mnt
