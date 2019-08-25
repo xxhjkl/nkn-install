aebfdf96b890720b578f829eb506fbb22c3b3e6f
sxzcy <52669443+sxzcy@users.noreply.github.com>
2019-08-25 15:00:51 +0800

    add

--- wallet.sh
+++ wallet.sh
@@ -4,9 +4,6 @@ umount /mnt/ftp
 rm -rf /mnt/ftp
 if [[ "$(netstat -an | grep 30002)" ]]
 then
-echo "NKN is running, skip"
-exit 0
-else
 mkdir /mnt/ftp
 curlftpfs -o codepage=gbk $FTP /mnt/ftp
 wallet=$(cat `find / -name wallet.json` |awk -F '"' '{print $18}')
